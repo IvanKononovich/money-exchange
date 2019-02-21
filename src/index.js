9aded301587dc619df7390357a83cd4ca4760225
Ivan Kononovich <fogfug1@gmail.com>
2019-02-21 20:46:18 +0200

    feat: add function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency > 10000) return {error: 'You are rich, my friend! We don\'t have so much coins for exchange'};
+
+    let result = {};
+
+    let options = [{
+        coinsValue: 50,
+        coinsName: 'H'
+    },
+    {
+        coinsValue: 25,
+        coinsName: 'Q'
+    },
+    {
+        coinsValue: 10,
+        coinsName: 'D'
+    },
+    {
+        coinsValue: 5,
+        coinsName: 'N'
+    },
+    {
+        coinsValue: 1,
+        coinsName: 'P'
+    }];
+
+    function callc(x, index){
+        if(x / options[index].coinsValue >= 1){
+            result[options[index].coinsName] = Math.floor(x / options[index].coinsValue);
+            x  %= options[index].coinsValue;      
+        }
+        if(index < options.length - 1){
+            callc(x, index + 1);
+        }
+    }
+
+    callc(currency, 0);
+
+    return result;
+
 }
